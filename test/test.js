b89b6bdff11212aa9a399d035b84d8a5ae648a2e
Mark Stahl <mjstahl@gmail.com>
2015-10-10 00:19:27 -0400

    Updated test to make HTTP request

--- test/test.js
+++ test/test.js
@@ -1,13 +1,16 @@
 var fs = require("fs");
 var cheerio = require("cheerio");
+var request = require("request");
 require("chai").should();
 
+var PARAMS = "?q1=programmer&l1=31406&q2=developer&l2=31406&q3=it+product+manager&l3=31406";
+var URL = "http://www.indeed.com/salary" + PARAMS;
 describe("salaries-indeed", function() {
 	var $ = "";
 	before(function(done) {
-		fs.readFile(__dirname + "/test-data.html", function(err, html) {
+		request(URL, function(err, response, body) {
 			if (err) return done(err);
-			$ = cheerio.load(html.toString());
+			$ = cheerio.load(body);
 			done();
 		});
 	});
