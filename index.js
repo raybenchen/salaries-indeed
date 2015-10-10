676928ff09e362ecf50b6066bd4cd48a67b6e2da
Mark Stahl <mjstahl@gmail.com>
2015-10-10 01:45:39 -0400

    More tests over interface and util functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+module.exports = (function() {
+	var self = {};
+	self.of = function(title, zip) {
+		if (!self.__jobs) { self.__jobs = []; }
+		self.__jobs.push({
+			"title": title,
+			"zip": zip
+		});
+		return self;
+	};
+	self.and = function(title, zip) {
+		return self.of(title, zip);
+	};
+	self.then = function(cb) {
 
+	};
+	return self;
+})();
--- test/test.js
+++ test/test.js
@@ -3,21 +3,63 @@ var cheerio = require("cheerio");
 var request = require("request");
 require("chai").should();
 
-var PARAMS = "?q1=programmer&l1=31406&q2=developer&l2=31406&q3=it+product+manager&l3=31406";
-var URL = "http://www.indeed.com/salary" + PARAMS;
-describe("salaries-indeed", function() {
+describe("salaries-indeed interface", function() {
+	var salary = require("../index");
+	describe("'and' interface", function() {
+		it("should have 1 job after calling 'and' before 'of'", function() {
+			salary.and("developer", 31419);
+			salary.should.have.property("__jobs");
+			salary.__jobs.should.be.length(1);
+		});
+		it("'and' should add job object with title and zip props", function() {
+			var jobs = salary.__jobs;
+			jobs[0].should.have.property("title").equal("developer");
+			jobs[0].should.have.property("zip").equal(31419);
+		});
+	});
+	describe("'of' interface", function() {
+		it("should have 2 job after calling 'of' after 'and'", function() {
+			salary.of("developer", 31419);
+			salary.should.have.property("__jobs");
+			salary.__jobs.should.be.length(2);
+		});
+		it("'of' should add job object with title and zip props", function() {
+			var jobs = salary.__jobs;
+			jobs[0].should.have.property("title").equal("developer");
+			jobs[0].should.have.property("zip").equal(31419);
+		});
+	});
+});
+
+describe("query generation", function() {
+	var salary = require("../index");
+	var util = require("../utils");
+	before(function() {
+		salary.__jobs = null;
+	});
+	it("should provide a query string given a salary object", function() {
+		salary.of("developer", 31419);
+		util.query(salary)
+			.should.equal("?q1=developer&l1=31419");
+	});
+	it("query should handle multiple job objects", function() {
+		salary.of("programmer", 31406);
+		util.query(salary)
+			.should.equal("?q1=developer&l1=31419&q2=programmer&l2=31406");
+	});
+});
+
+describe("indeed HTML parsing", function() {
+	this.timeout(5000);
+
+	var PARAMS = "?q1=programmer&l1=31406&q2=developer&l2=31406&q3=it+product+manager&l3=31406";
+	var URL = "http://www.indeed.com/salary" + PARAMS;
 	var $ = "";
 	before(function(done) {
 		request(URL, function(err, response, body) {
 			if (err) return done(err);
 			$ = cheerio.load(body);
 			done();
-		});
-	});
-	describe("parse salary table", function() {
-		it("should have three rows", function() {
-			var rows = $("table#salary_display_table > tbody > tr");
-			rows.should.have.length(3);
 		});
 	});
 });
